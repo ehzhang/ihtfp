2a76c00bf7fa8470b6cdad8abbfa4dde3b215392
Merry Mou <mmou@mit.edu>
2014-01-14 23:24:43 -0500

    make posts show up in grid.

--- ihtfp.js
+++ ihtfp.js
@@ -4,19 +4,29 @@ if (Meteor.isClient) {
     console.log("I GOT THE FEELS! " + Feels.find().count());
   });
 
+  Template.grid.feels = function () {
+    return Feels.find();
+  }
+
+  Template.post.clear = function () {
+  // reset post stuff
+  }
+
   Template.post.events({
     'click .green': function () {
-      var username = $("textarea").val();
-      var text = $(":input:text").val();
+      var username = $(":input:text").val();
+      var text = $("textarea").val();
       var emotion = $('.green').html();
       Meteor.call("postFeel", username, text, emotion);
+      // reset post stuff
       return false;
     },
     'click .black': function () {
-      var username = $("textarea").val();
-      var text = $(":input:text").val();
+      var username = $(":input:text").val();
+      var text = $("textarea").val();
       var emotion = $('.black').html();
       Meteor.call("postFeel", username, text, emotion);
+      // reset post stuff
       return false;
     }
   });
@@ -24,7 +34,7 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.publish("feels", function () {
-    console.log("I'M SENDING THE FEELS! " + Feels.find({username: "Bob"}).count());
-    return Feels.find({username: "Bob"});
+    console.log("I'M SENDING THE FEELS! " + Feels.find().count());
+    return Feels.find({username :"Bob"});
   });
 }
