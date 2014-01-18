9b4df4e5e557dcbd90fd921e82a0d53865450723
Merry Mou <moumerry@gmail.com>
2014-01-18 18:46:16 -0500

    did a bit of stuff on splash page, but nothing done. pushing only so that i can pull.

--- client/js/splash.js
+++ client/js/splash.js
@@ -1,7 +1,7 @@
 // Custom javascript for the ihtfp site
 
-$(window).load(function(){
-  var num = Math.floor(Math.random()*3)+1;
+$(window).load(function () {
+  var num = Math.floor(Math.random() * 3) + 1;
   var randomGif = 'url(\'/feels_gifs/' + num + '.gif\')' + ' no-repeat center center fixed';
-  $('#splash').css('background', randomGif).css('background-size', 'cover');
+  $('#splash').css('background', randomGif).css('background-size', 'cover').css('-webkit-background-size', 'cover').css('-moz-background-size', 'cover').css('-o-background-size', 'cover');
 });
