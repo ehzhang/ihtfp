ca61aa1c6251a7344308fc1bcc39cd92444e2d2e
Edwin Zhang <ehzhang@mit.edu>
2014-01-28 04:07:57 -0500

    post validation. limit post frequency/size, moar security

--- client/js/composer.js
+++ client/js/composer.js
@@ -35,24 +35,59 @@ Template.post.rendered = function () {
 
 /**
  * Events for the post template, inside the composer.
+ *
  */
 Template.post.events({
   'click .emotion.submit.button': function () {
-    var username = Meteor.user().username;
-    var text = $("textarea").val();
+    var username = Meteor.user().username,
+        text = $("textarea").val();
+    // Quick validate text length on client side
+    if (text.length > 1200) {
+      return false;
+    }
     var emotion = Session.get("emotion");
-    Meteor.call("postFeel", username, text, emotion);
-    // reset post stuff
-    $('#composer').dimmer('hide');
-    $('textarea').val("");
+    Meteor.call("postFeel", username, text, emotion,
+      function(error, result) {
+        if (result[0]) {
+          // Successful post!
+          // reset post stuff
+          $('#composer').dimmer('hide');
+          $('textarea').val("");
+        } else {
+          // In case of error, get the error.
+          $('#post-warning').html(result[1]);
+          $('.emotion.editor .field').addClass("error");
+        }
+      });
     return false;
   },
   'click .emotion .item': function (event) {
     var emotion = $(event.target).attr('emotion');
     Session.set("emotion", emotion);
+    return false;
   },
   'click .cancel.button': function () {
-    $('#composer').dimmer('hide');
+    $('#composer')
+      .dimmer('hide');
     $('textarea').val("");
+    $('#post-warning').html("");
+    $('.emotion.editor .field').removeClass("error");
+    return false;
+  },
+  'keyup .emotion.editor .field' : function () {
+    // Error handling for invalid posts.
+    var $emotionEditor = $('textarea'),
+        $submitButton = $('.emotion.submit.button'),
+        $postWarning = $('#post-warning');
+    if ($emotionEditor.val().length > 1200) {
+      $postWarning.text('Your feel exceeds the character limit!');
+      $emotionEditor.parent().addClass("error");
+      $submitButton.addClass("disabled");
+    } else {
+      $postWarning.text("");
+      $emotionEditor.parent().removeClass("error");
+      $submitButton.removeClass("disabled");
+    }
+    return false;
   }
 });
--- server/model.js
+++ server/model.js
@@ -21,7 +21,47 @@ Feels = new Meteor.Collection("feels");
 
 Meteor.methods({
   postFeel: function (username, text, emotion) {
-    Feels.insert(
+
+    // First check that the username and userId match.
+    var user = Meteor.users.find({_id: this.userId}, {fields: {username: 1}}).fetch()[0];
+    if (user.username !== username) {
+      return [false, "Oops. You're not who you say you are."];
+    }
+
+    // Check to see if the user has posted recently, and how often.
+    // Currently limit to 6 posts an hour.
+    var latestPosts = Feels.find(
+      {
+        username: username
+      },{
+        sort:{timestamp: -1},
+        limit: 6
+      }).fetch();
+
+    // Make sure there are even posts there to limit.
+    if (latestPosts.length >= 6) {
+      var latestPost = latestPosts[latestPosts.length -1];
+      var minutesDiff = Math.abs(new Date - latestPost.timestamp)/60000;
+      if (minutesDiff < 60) {
+        return [false, "Oops. Looks like you've had too many feels in the past hour. " +
+          "<br> Try posting again a little later!"];
+      }
+    }
+
+    // Shouldn't happen, but if somebody sneaks by with text larger than the limit.
+    if (text.length > 1200) {
+      return [false, "Your feel exceeds the 1200 character limit!" +
+        "<br> Also, get the fuck out of the console!"];
+    }
+
+    // Also like, seriously shouldn't happen.
+    if (!(emotions.indexOf(emotion) >= 0)) {
+      return [false, "That's not an emotion I've seen before." +
+        "<br> Get out of the console, yo!"]
+    }
+
+    // If not posting too frequently/not posted yet, post that shittt
+    return Feels.insert(
       {
         username: username,
         anon: true, // default for now
@@ -30,7 +70,8 @@ Meteor.methods({
         hearts: 0,
         timestamp: new Date()
       }, function () {
-        console.log("successfully posted!!")
+        console.log("successfully posted!!");
+        return [true, "Successfully Posted!"]
       });
   }
 });
